478fdda4888a80af7d0878e658027d10fc675973
ddord <ikihiji11@gmail.com>
2022-10-19 18:38:03 +0900

    update

--- CloudNativeWebApplications/CloudNativeWebApplications/Controllers/HomeController.cs
+++ CloudNativeWebApplications/CloudNativeWebApplications/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authentication;
+﻿using CloudNativeWebApplications.Common;
+using CloudNativeWebApplications.Services;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -11,6 +13,13 @@ namespace CloudNativeWebApplications.Controllers
 {
     public class HomeController : Controller
     {
+        private CommonService _commonService;
+
+        public HomeController()
+        {
+            _commonService = new CommonService();
+        }
+
         public IActionResult Login()
         {
             return View();
@@ -31,43 +40,24 @@ namespace CloudNativeWebApplications.Controllers
             if (string.IsNullOrWhiteSpace(id))
             {
                 returnResult = false;
-                returnMsg = "아이디를 입력하세요.";
+                returnMsg = "Please enter ID.";
             }
             else if (string.IsNullOrWhiteSpace(password))
             {
                 returnResult = false;
-                returnMsg = "패스워드를 입력하세요.";
+                returnMsg = "Please enter Password.";
             }
             else
             {
-                var adminUserList = _commonDac.SelectAdminUser(id);
+                var adminUserList = _commonService.SelectAdminUser(id);
                 List<object> list = (List<object>)adminUserList;
                 if (list.Count == 0)
                 {
                     returnResult = false;
-                    returnMsg = "관리자로 등록되지 않은 사용자 입니다.";
+                    returnMsg = "Unregistered user.";
                 }
                 else
                 {
-                    string upn = null;
-#if !DEBUG
-                    try
-                    {
-                        upn = ActiveDirectoryManager.GetUserPrincipalName(id, password);
-                        if (string.IsNullOrEmpty(upn))
-                        {
-                            returnResult = false;
-                            returnMsg = "Active Directory에서 사용자를 찾을 수 없습니다.";
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        MethodBase method = MethodInfo.GetCurrentMethod();
-                        FileLogWriter.WriteErrorLog(string.Format("{0}.{1}", method.DeclaringType.ToString(), method.Name), string.Empty, ex);
-                        returnResult = false;
-                        returnMsg = "패스워드를 확인 해주세요";
-                    }
-#endif
 
                     if (returnResult)
                     {
